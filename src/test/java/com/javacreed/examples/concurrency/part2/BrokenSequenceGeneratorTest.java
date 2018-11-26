849de451a5bca2c08330e701e51c94ffbf6934ea
Alber Attard <albert@javacreed.com>
2018-11-26 07:39:53 +0100

    Cleanup

--- src/test/java/com/javacreed/examples/concurrency/part2/BrokenSequenceGeneratorTest.java
+++ src/test/java/com/javacreed/examples/concurrency/part2/BrokenSequenceGeneratorTest.java
@@ -1,24 +1,3 @@
-/*
- * #%L
- * The Broken Singleton
- * $Id:$
- * $HeadURL:$
- * %%
- * Copyright (C) 2012 - 2015 Java Creed
- * %%
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * 
- *      http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- * #L%
- */
 package com.javacreed.examples.concurrency.part2;
 
 import java.util.Collections;
@@ -31,10 +10,10 @@ import java.util.concurrent.BrokenBarrierException;
 import java.util.concurrent.CyclicBarrier;
 import java.util.concurrent.atomic.AtomicReference;
 
-import junit.framework.AssertionFailedError;
-
 import org.junit.Assert;
 import org.junit.Test;
+
+import junit.framework.AssertionFailedError;
 
 public class BrokenSequenceGeneratorTest {
 
@@ -50,7 +29,7 @@ public class BrokenSequenceGeneratorTest {
     final Set<Long> generatedValues = new HashSet<>(size);
 
     final Set<BrokenSequenceGenerator> instances = Collections
-        .newSetFromMap(new IdentityHashMap<BrokenSequenceGenerator, Boolean>());
+                                                              .newSetFromMap(new IdentityHashMap<BrokenSequenceGenerator, Boolean>());
 
     final List<Thread> threads = new LinkedList<>();
     for (int i = 0; i < size; i++) {
